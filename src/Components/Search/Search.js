9353148b9adcb10361006895116cd352cbc85fbf
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2022-01-03 19:43:33 +0530

    Added content for Search page

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -4,7 +4,16 @@ import './Search.css';
 const Search = () => {
     return (
         <div className="searchSection">
-            <h1>Search</h1>
+            <h1 className="sectionHeading">Search by Area</h1>
+            <hr />
+            <p className="description">Need help in getting more insights about users?</p>
+            <p className="description">Filter out an area from the dropdown below to get more details about the users from that area in Bangalore.</p>
+            <form>
+            <select className="areaDropdown">
+            <option value="Koramangala">Koramangala</option>
+            </select>
+            <button className="searchButton">Search</button>
+            </form>
         </div>
     )
 }
