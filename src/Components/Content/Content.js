87f618a5d3bc3987bd3e29a313f427dffc0a89e3
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2022-01-03 20:38:47 +0530

    Committing changes made for displaying area

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import Map from '../Map/Map';
 import Search from '../Search/Search';
 import './Content.css';
 
+export const ValuesContext = React.createContext();
+
 const Content = () => {
+
+    const [areas, setAreas] = useState([]);
+    const [users, setUsers] = useState([]);
+
+    const fetchingData = async() =>{
+        const urls = ["https://kyupid-api.vercel.app/api/areas", "https://kyupid-api.vercel.app/api/users"];
+        const resp = await Promise.all(urls.map(url => fetch(url)));
+        const data = await Promise.all(resp.map(response => response.json()));
+        setAreas(data[0].features);
+        setUsers(data[1]);
+    }
+
+    useEffect(()=>{
+        fetchingData();
+    }, [])
+
     return (
         <div className="content">
             <div className="contentModal">
+            <ValuesContext.Provider value={{areas, users}}>
             <Search/>
             <Map/>
+            </ValuesContext.Provider>
             </div>
         </div>
     )
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {ValuesContext} from '../Content/Content';
 import './Search.css';
 
 const Search = () => {
+
+    const {areas} = useContext(ValuesContext);
+
     return (
         <div className="searchSection">
             <h1 className="sectionHeading">Search by Area</h1>
@@ -10,7 +14,10 @@ const Search = () => {
             <p className="description">Filter out an area from the dropdown below to get more details about the users from that area in Bangalore.</p>
             <form>
             <select className="areaDropdown">
-            <option value="Koramangala">Koramangala</option>
+            <option value="null">Please select an area</option>
+            {areas.map((area)=>{
+                return <option value={area.properties.name} key={area.properties.area_id}>{area.properties.name}</option>
+            })}
             </select>
             <button className="searchButton">Search</button>
             </form>
